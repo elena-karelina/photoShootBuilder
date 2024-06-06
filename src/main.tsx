c26112dbb6a9eea5f9e20df5723aeb6be7d4778f
Elena Karelina <karelinae03@mail.ru>
2024-06-06 20:32:15 +0700

    исправлена маршрутизация

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import { createRoot } from "react-dom/client";
-
+import * as React from "react";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import router from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <>
-    <RouterProvider router={router} />
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
   </>
 );
